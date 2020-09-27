dd237aa07b25f7fa42e235ebcd9a7b1d83d70900
Ludovic Piot <ludovic.piot@thegaragebandofit.com>
2020-09-27 16:14:34 +0200

    :sparkles: change the S3 bucket into a static Web site

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,5 +25,23 @@ provider "aws" {
 resource "aws_s3_bucket" "lpiot_bucket" {
   bucket = "s3-website-test.esgi.lpiot"
   acl    = "public-read"
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_object" "static_www_index" {
+  bucket = aws_s3_bucket.lpiot_bucket.bucket
+  key    = "index.html"
+  source = "../resources/index.html"
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_object" "static_www_error" {
+  bucket = aws_s3_bucket.lpiot_bucket.bucket
+  key    = "error.html"
+  source = "../resources/error.html"
+  acl    = "public-read"
 }
 
